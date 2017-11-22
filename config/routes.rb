42cc94043a05890af033e68f4e33dc9722ee2e83
Luci-Lawless <lu.gmmm@gmail.com>
2017-11-22 21:49:12 +0100

    Create Pair model, controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   devise_for :users
+
+  resources :users
+  resources :pairs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
