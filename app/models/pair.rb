087a631fa82d8dfef515655ebea824fccda28690
Luci-Lawless <lu.gmmm@gmail.com>
2017-11-23 23:00:52 +0100

    Add association for pair with two students

--- app/models/pair.rb
+++ app/models/pair.rb
@@ -1,24 +1,4 @@
 class Pair < ApplicationRecord
-belongs_to :user
-
-  @students = User.where(is_admin:false)
-
-  def generate_pairs
-    @students = []
-
-    student = @students.dup
-    fixed_student = student.shuffle!.pop
-
-    student.size.times do |x|
-      puts "Day #{x+1}"
-      two_rows = [[fixed_student]+student[0..student.size/2-1],
-      student[student.size/2..-1].reverse]
-      pairs = two_rows.transpose.shuffle
-      pairs.each do {|i| p i}
-        student.rotate!
-        puts student
-      end
-
-  end
-
-  end
+  belongs_to :student1, :class_name => 'User'
+  belongs_to :student2, :class_name => 'User'
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,48 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Pair.destroy_all
+
+#Users
+user1 = User.create!(
+  email: "user1@email.com",
+  name: "User1 Name",
+  password: "123123"
+)
+user2 = User.create!(
+  email: "user2@email.com",
+  name: "User2 Name",
+  password: "123123"
+)
+user3 = User.create!(
+  email: "user3@email.com",
+  name: "User3 Name",
+  password: "123123"
+)
+user4 = User.create!(
+  email: "user4@email.com",
+  name: "User4 Name",
+  password: "123123"
+)
+
+#Pairs
+
+pairs = Pair.create!([
+{
+  day: "2017-11-12",
+  student1: user1,
+  student2: user2
+},
+{
+  day: "2017-11-12",
+  student1: user3,
+  student2: user4
+},
+{
+  day: "2017-11-13",
+  student1: user1,
+  student2: user3
+},
+{
+  day: "2017-11-13",
+  student1: user2,
+  student2: user4
+}])
