67540943c7e9c6f75ec8678969dc1881188514cb
Luci-Lawless <lu.gmmm@gmail.com>
2017-11-23 11:56:43 +0100

    Create one to many association

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def set_authorization
+
+    if @user.is_admin?
+      redirect_to @pairs.admin_index
+    else
+      redirect_to @pairs.user_index
+   end
+  end
 end
--- app/models/pair.rb
+++ app/models/pair.rb
@@ -1,2 +1,17 @@
 class Pair < ApplicationRecord
+  def create_pairs
+    names = ["Luciana", "Isis", "Henock", "Veranika", "Tea", "Ward", "Rayta", "Ha", "Karla", "Daniil", "Anna", "Gabrijela", "Jesse", "Kyriakos", "Lara", "Lianne", "Jason", "Robin"]
+
+    n = names.dup
+    fixed_name = n.shuffle!.pop
+
+    n.size.times do |i|
+      puts "Day #{i+1}"
+      two_rows = [[fixed_name]+n[0..n.size/2-1], n[n.size/2..-1].reverse]
+      pairs = two_rows.transpose.shuffle
+      pairs.each{|c| p c }
+      n.rotate!
+      puts
+    end
+  end
 end
